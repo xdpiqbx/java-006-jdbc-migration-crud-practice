975748ebcf854a002baf136b145ec3ae25788418
xdpiqbx <xdpiqbx@gmail.com>
2022-12-15 17:07:01 +0200

    added flyway

--- app/src/main/java/cw/train/App.java
+++ app/src/main/java/cw/train/App.java
@@ -3,12 +3,11 @@
  */
 package cw.train;
 
+import cw.train.storage.DatabaseInitService;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        DatabaseInitService.initDb();
     }
 }
--- app/src/main/java/cw/train/storage/StorageConstants.java
+++ app/src/main/java/cw/train/storage/StorageConstants.java
@@ -2,5 +2,5 @@ package cw.train.storage;
 
 public class StorageConstants {
 //    public static final String CONNECTION_URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1"; // connection for tests
-    public static final String CONNECTION_URL = "jdbc:h2:space_train";
+    public static final String CONNECTION_URL = "jdbc:h2:./space_train";
 }
